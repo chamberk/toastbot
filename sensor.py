ec2b48ed2b4bb794f892b6b4f8d011c33a1ffa78
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-01-15 09:02:38 -0600

    loop sensor

--- sensor.py
+++ sensor.py
@@ -3,68 +3,75 @@ import time
 import sys
 import toast_request as toast
 
-loopcount = 0
+toastLoopBool = True
 
-GPIO.setmode(GPIO.BCM)
+while toastLoopBool:
+    toastLoop()
 
-#Variables for pins
-TRIG = 20
-ECHO = 26
+# FUNCTIONS !!
+def toastLoop():
+    loopcount = 0
 
-#Pin setup for input and output
-# print("Distance Measurement in Progress")
+    GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+    #Variables for pins
+    TRIG = 20
+    ECHO = 26
 
+    #Pin setup for input and output
+    # print("Distance Measurement in Progress")
 
-# Each shot works by sending a 10-microsecond pulse at around 40khz,
-# marking the time at which the pulse is sent and then,
-# subsequentially, when the reflected signal is detected.
-# To ensure accuracy, we must first settle the trigger and wait:
-GPIO.output(TRIG,False)
-print("Waiting for sensor to settle.")
-
-time.sleep(2)
-
-#Continuously checking for range
-while True:
- GPIO.output(TRIG, True)
-# print("inside checking range")
- time.sleep(0.1)
- GPIO.output(TRIG, False)
- 
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
 
 
-# waiting the Echo
-# print("before pulse start")  # debug statement
- pulse_start = time.time()
- while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+    # Each shot works by sending a 10-microsecond pulse at around 40khz,
+    # marking the time at which the pulse is sent and then,
+    # subsequentially, when the reflected signal is detected.
+    # To ensure accuracy, we must first settle the trigger and wait:
+    GPIO.output(TRIG,False)
+    print("Waiting for sensor to settle.")
 
-# Pulse received
- while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
- #  print("after pulse")  # debug statement
-  pulse_duration = pulse_end - pulse_start
+    time.sleep(2)
 
-# Calculating distance
- distance = pulse_duration*17150
- distance = round(distance, 2)
- loopcount+=1
+    #Continuously checking for range
+    while True:
+     GPIO.output(TRIG, True)
+    # print("inside checking range")
+     time.sleep(0.1)
+     GPIO.output(TRIG, False)
 
-#Publish distance to some other thing
- toastCount = 0
- if distance > 3000:
-  toastCount+=1
-  print("TOAST IS DONEEEEEEEE")
-  toast.sendsms_task()
-  print("SMS SENT")
-  GPIO.cleanup()
-  break;
- else:
-  toastCount = 0
 
-# clean up and log out
-#GPIO.cleanup()
-sys.exit()
+
+    # waiting the Echo
+    # print("before pulse start")  # debug statement
+     pulse_start = time.time()
+     while GPIO.input(ECHO)==0:
+      pulse_start = time.time()
+
+    # Pulse received
+     while GPIO.input(ECHO)==1:
+      pulse_end = time.time()
+     #  print("after pulse")  # debug statement
+      pulse_duration = pulse_end - pulse_start
+
+    # Calculating distance
+     distance = pulse_duration*17150
+     distance = round(distance, 2)
+     loopcount+=1
+
+    #Publish distance to some other thing
+     toastCount = 0
+     if distance > 3000:
+      toastCount+=1
+      print("TOAST IS DONEEEEEEEE")
+      toast.sendsms_task()
+      print("SMS SENT")
+      GPIO.cleanup()
+      break;
+     else:
+      toastCount = 0
+
+    # clean up and log out
+    #GPIO.cleanup()
+    sys.exit()
